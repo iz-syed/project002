2058b9bdc44d42e94d9187a6a1f035dfc44d5f5f
iz-syed <mubarakcc7@gmail.com>
2020-06-29 22:09:37 +0530

    initiate blank & full components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlankComponent } from './layouts/blank/blank.component';
+import { FullComponent } from './layouts/full/full.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BlankComponent
+  },
+  {
+    path:'',
+    component: FullComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
